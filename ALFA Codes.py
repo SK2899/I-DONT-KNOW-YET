74f4e5477256c4cf9d843b1d20a839818e169575
Salwan <40645574+SK2899@users.noreply.github.com>
2018-08-02 13:19:00 +0300

    I Added Two Files For This Project
    I'm trying to build a codes keyboard app for windows I have the idea but I don't have the skills yet.

---
+++ ALFA Codes.py
@@ -0,0 +1,35 @@
+def ALFAc():
+    ALFAdic = {
+    'A' : 'alfa',
+    'B' : 'bravo',
+    'C' : 'charlie',
+    'D' : 'delta',
+    'E' : 'echo',
+    'F' : 'foxtrot',
+    'G' : 'golf',
+    'H' : 'hotel',
+    'I' : 'india',
+    'J' : 'juliett',
+    'K' : 'kilo',
+    'L' : 'lima',
+    'M' : 'mike',
+    'N' : 'november',
+    'O' : 'oscar',
+    'P' : 'papa',
+    'Q' : 'quebec',
+    'R' : 'romeo',
+    'S' : 'sierra',
+    'T' : 'tango',
+    'U' : 'uniform',
+    'V' : 'victor',
+    'W' : 'whiskey',
+    'X' : 'x-ray',
+    'Y' : 'yankee',
+    'Z' : 'zulu',
+    }
+    return ALFAdic
+
+ALFAc()
+
+
+
